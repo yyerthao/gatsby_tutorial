c0d6a52787a348478faf001b3ec4798b57d02a4a
yyerthao <yyerrthao@gmail.com>
2021-07-20 14:06:26 -0500

    Adding onClick onto button

--- src/components/movie.js
+++ src/components/movie.js
@@ -6,12 +6,20 @@ export default class Movie extends React.Component {
         super(props);
 
         this.state = {}
+
+        this.handleClick = this.onClick.bind(this);
     }
+
+    onClick(){
+        alert("Hello")
+    }// end constructor
+    
 
     render(){
         return(
             <div>
                 <h1>{this.props.data.title}</h1>
+                <button onClick={this.handleClick}>Click Me</button>
                 <p>{this.props.data.genre}</p>
                 <img src={this.props.data.image} alt={this.props.data.title}/>
                 <iframe width="560" height="315" src={`https://www.youtube.com/embed/${this.props.data.trailer}`} 
